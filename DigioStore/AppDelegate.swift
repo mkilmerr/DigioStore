f30c0d45338e8ad9fa4af8802f5c92ff38d16904
kilmer <kilmer@Carloss-MacBook-Pro.local>
2024-09-05 10:57:56 -0300

    Project initial structure created

---
+++ DigioStore/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DigioStore
+//
+//  Created by kilmer on 05/09/24.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        let initialViewController = UIViewController()
+
+        window?.rootViewController = initialViewController
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+}
